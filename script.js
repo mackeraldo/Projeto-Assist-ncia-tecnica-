d11d3bf8a05d789b57f60475959692864bcb0920
Thiago <92761953+mackeraldo@users.noreply.github.com>
2023-03-21 23:48:04 -0300

    Ordem de Serviço Assistência Técnica
    Uma ordem de serviço é um documento formal que descreve o trabalho a ser realizado para um cliente. Ele autoriza e direciona uma empresa ou um grupo de profissionais a desempenhar determinada tarefa.
    
    Utilizei HTML, CSS e Javascript pra manipular o DOM, esse são meus primeiros passos no aprendizado na aréa  de desenvolvimento.

---
+++ script.js
@@ -0,0 +1,56 @@
+function receber_cliente_1() {
+  var cliente01 = document.getElementById('cliente_1').value
+  console.log(cliente01)
+
+  document.getElementById('recebe_valor_cliente').innerHTML = cliente01
+}
+
+function receber_telefone_1() {
+  var telefone_1 = document.getElementById('telefone_1').value
+  console.log(telefone_1)
+
+  document.getElementById('recebe_valor_telefone').innerHTML = telefone_1
+}
+
+function receber_endereco() {
+  var endereco_1 = document.getElementById('endereco_1').value
+  console.log(endereco_1)
+
+  document.getElementById('recebe_endereco').innerHTML = endereco_1
+}
+
+function recebe_bairro_cliente() {
+  var bairro_cliente = document.getElementById('bairro_cliente').value
+  console.log(bairro_cliente)
+
+  document.getElementById('recebe_bairro').innerHTML = bairro_cliente
+}
+
+
+function receber_marca_1() {
+  var marca_1 = document.getElementById('marca_1').value
+  console.log(marca_1)
+
+  document.getElementById('recebe_marca_aparelho').innerHTML = marca_1
+}
+
+function receber_modelo_1() {
+  var modelo_1 = document.getElementById('modelo_1').value
+  console.log(modelo_1)
+
+  document.getElementById('recebe_valor_modelo').innerHTML = modelo_1
+}
+
+function receber_problema_1() {
+  var problema_1 = document.getElementById('problema_1').value
+  console.log(problema_1)
+
+  document.getElementById('recebe_valor_defeito').innerHTML = problema_1
+}
+
+function receber_valor_1() {
+  var valor_1 = document.getElementById('valor_1').value
+  console.log(valor_1)
+
+  document.getElementById('recebe_valor').innerHTML = valor_1
+}
